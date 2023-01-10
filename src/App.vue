2f0598183f0f9b8db74c4b005d9613ab07487f54
llzzmm645 <liuzimo_645@163.com>
2023-01-10 19:46:20 +0800

    First Commit.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <hello />
+</template>
+
+<script>
+import hello from "./components/hello.vue"
+export default {
+  name: 'App',
+  components:{hello}
+}
+</script>
---
+++ src/components/hello.vue
@@ -0,0 +1,27 @@
+<template>
+    <from @submit.prevent="postdata">
+        名字:<input type="text" v-model="name" id="name">
+        <br>
+        年龄:<input type="text" v-model="age" id="age">
+        <br>
+        幸运数字:<input type="text" v-model="lucknumber" id="lucknumber">
+        <br>
+        <input type="submit" value="提交">
+    </from>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            name:"Liuzimo",
+            age:11,
+            lucknumber:5
+        }
+    },
+    methods:{
+        postdata(){
+            alert(this.name+" - "+this.$data.age.toString()+" - "+this.lucknumber.toString())
+        }
+    }
+}
+</script>
